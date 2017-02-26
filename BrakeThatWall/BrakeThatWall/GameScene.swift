739f3afc7532ee9b46ed71b19e825c613f6cc01b
Carlo Albino <carloalbino@gmail.com>
2017-02-26 17:27:36 -0500

    Base game commit

---
+++ BrakeThatWall/BrakeThatWall/GameScene.swift
@@ -0,0 +1,88 @@
+//
+//  GameScene.swift
+//  BrakeThatWall
+//
+//  Created by Carlo Albino on 2017-02-26.
+//  Copyright © 2017 Carlo Albino. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    var playerPaddle = SKSpriteNode()
+    var ball = SKSpriteNode()
+    
+    var ballPower : Int = 20
+    
+    var isTouching = false
+    
+    override func didMove(to view: SKView) {
+        playerPaddle = self.childNode(withName: "playerPaddle") as! SKSpriteNode
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        
+        ball.physicsBody?.applyImpulse(CGVector(dx: -ballPower, dy: -ballPower))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        
+        border.friction = 0;
+        border.restitution = 1;
+        
+        self.physicsBody = border
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches
+        {
+            let location = touch.location(in: self)
+            
+            playerPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+            
+            isTouching = true
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches
+        {
+            let location = touch.location(in: self)
+            
+            playerPaddle.run(SKAction.moveTo(x: location.x, duration: 0.05))
+            
+            isTouching = true
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isTouching = false
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isTouching = false
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if(isTouching == false)
+        {
+            playerPaddle.run(SKAction.moveTo(x: ball.position.x, duration: 0.05))
+        }
+        
+        // How to detect collisions on specific objects?
+        for node in (ball.physicsBody?.allContactedBodies())!
+        {
+            if(node.self == playerPaddle || node.self == self)
+            {
+                let randomDir = CGFloat(arc4random_uniform(10)) - 5
+                
+                let dirMagnitude : Float = sqrt(pow(Float((ball.physicsBody?.velocity.dx)!), 2) + pow(Float((ball.physicsBody?.velocity.dy)!), 2))
+                let x : Float = Float((ball.physicsBody?.velocity.dx)!)
+                let y : Float = Float((ball.physicsBody?.velocity.dy)!)
+                
+                let dir : CGVector = CGVector(dx: CGFloat((x / dirMagnitude) * Float(ballPower)), dy: CGFloat((y / dirMagnitude) * Float(ballPower)))
+                
+                ball.physicsBody?.applyImpulse(CGVector(dx: dir.dx + randomDir, dy: dir.dy + randomDir))
+            }
+        }
+    }
+}
